b93ded28484da138a7991a11553def00ab9f08a9
fang.yang <yangfang2028@126.com>
2021-01-09 13:40:14 +0800

    ✨ 修改打包输出路径

---
+++ docs/precache-manifest.d628181b6d4139523565baef2220c298.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "306afde898bde1d4940efe6aee99da79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edb656b1094d4e806db8",
+    "url": "/static/css/2.73e40670.chunk.css"
+  },
+  {
+    "revision": "c66320282b1b36f5f872",
+    "url": "/static/css/main.2691ef95.chunk.css"
+  },
+  {
+    "revision": "edb656b1094d4e806db8",
+    "url": "/static/js/2.4462ea0f.chunk.js"
+  },
+  {
+    "revision": "7d6f66723a27bec5f2855aa663da7912",
+    "url": "/static/js/2.4462ea0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c66320282b1b36f5f872",
+    "url": "/static/js/main.bb018439.chunk.js"
+  },
+  {
+    "revision": "4b323feba20ae4fa70f8",
+    "url": "/static/js/runtime-main.5983213e.js"
+  }
+]);
